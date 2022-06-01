a9e42a914bb274ee70e69e38767a842a6d1cc3ec
Aryan7901 <85547188+Aryan7901@users.noreply.github.com>
2022-06-01 22:29:29 +0530

    moved to vite

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
+import svgrPlugin from "vite-plugin-svgr";
+import envCompatible from "vite-plugin-env-compatible";
+const ENV_PREFIX = "REACT_APP_";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // This changes the out put dir from dist to build
+  // comment this out if that isn't relevant for your project
+  esbuild: {
+    jsxFactory: "_jsx",
+    jsxFragment: "_jsxFragment",
+    jsxInject: `import { createElement as _jsx, Fragment as _jsxFragment } from 'react'`,
+  },
+  build: {
+    outDir: "build",
+  },
+  plugins: [
+    reactRefresh(),
+    svgrPlugin({
+      svgrOptions: {
+        icon: true,
+        // ...svgr options (https://react-svgr.com/docs/options/)
+      },
+    }),
+    envCompatible({ prefix: ENV_PREFIX }),
+  ],
+});
